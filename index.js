6273aab3d5f893bb2a54c4347485ef598629bb1a
HCP2024 <highercircuitprofessionals@gmail.com>
2025-06-26 20:28:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-const menuButton = document.querySelector("button.menu"); // Changed selector
+const searchDiv = document.querySelector("button.menu");
 const header = document.querySelector('header#academy-header .main');
-let isOpen = false;
+let searching = false;
 
 function handleHeader() {
-    isOpen = !isOpen; // Toggle the state
-    
-    if (isOpen) {
-        header.classList.add('active');
-    } else {
-        header.classList.remove('active');
-    }
 
-    // Update button content
-    menuButton.innerHTML = isOpen ? "X" : `<svg
-        xmlns="http://www.w3.org/2000/svg"
-        height="25"
-        viewBox="0 -960 960 960"
-        width="25"
-        class="menu"
-      >
-        <path
-          d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"
-        />
-      </svg>`;
+    const isOpen = header.classList.contains('active');
+    if (isOpen) {
+        header.classList.remove('active');
+        searching = header.classList.contains('active');
+    } else {
+        header.classList.add('active');
+        searching = header.classList.contains('active');
+    };
+
+    searchDiv.innerHTML = searching ? "X" : ` <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="25"
+            viewBox="0 -960 960 960"
+            width="25"
+            class="menu"
+          >
+            <path
+              d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"
+            />
+          </svg>`;
 }
 
-menuButton.addEventListener('click', handleHeader);
+searchDiv.addEventListener('click', () => {
+    handleHeader();
+});
 
 header.addEventListener('mouseleave', () => {
-    if (isOpen) {
+    if (searching) {
         handleHeader();
     }
 });
